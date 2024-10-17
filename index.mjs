6fa2bbdc5d57f553d7e43a08b3b4e8fa1cf8bdf5
aniel-aguero <zarpanaguero4@gmail.com>
2024-10-17 11:32:32 -0300

    eliminando archivos

---
+++ index.mjs
@@ -0,0 +1,30 @@
+
+import http from 'http';
+
+const hostname = '127.0.0.1';
+
+const port = 3000;
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.end(`Hola Mundo`);
+});
+
+server.listen(port, hostname,()=>{
+    console.log(`El servidor se esta ejecutando en http://${hostname}:${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log(`mensaje rapido`)
