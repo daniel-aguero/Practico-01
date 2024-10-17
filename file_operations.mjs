9bccd4f2de49d95d9a1057eed1d7875a0dc3585b
aniel-aguero <zarpanaguero4@gmail.com>
2024-10-17 13:15:51 -0300

    3er Ejecicio

---
+++ file_operations.mjs
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+const data = 'Este texto es un ejemplo. \n';
+
+fs.writeFile('ejemplo.txt', data, (err)=>{
+    if(err) throw err;
+    console.log(`El archivo ha sido guardado.`);
+
+    fs.readFile('ejemplo.txt', 'utf8', (err, content)=>{
+        if(err) throw err;
+        console.log(`Contenido del archivo:`);
+        console.log(content);
+    })
+})
